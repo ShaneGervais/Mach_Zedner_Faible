9f6807ab24247553082329732a77c4683b34b5d3
Shane Gervais <shanegervais16@gmail.com>
2023-06-19 15:55:53 -0300

    power_spectrum added "needs work"

--- plot.py
+++ plot.py
@@ -36,3 +36,44 @@ plt.xlabel("Time")
 plt.ylabel("Coherence")
 plt.savefig('degree_of_coherence.png')
 n=n+1
+
+pre_intensity_fft = np.fft.fft(pre_intensity)
+pre_magnitude_spectrum = np.abs(pre_intensity_fft)
+pre_power_spectrum = np.abs(pre_intensity_fft) ** 2
+
+post_intensity_fft = np.fft.fft(post_intensity)
+post_magnitude_spectrum = np.abs(post_intensity_fft)
+post_power_spectrum = np.abs(post_intensity_fft) ** 2
+
+
+plt.figure(n)
+sb.lineplot(pre_magnitude_spectrum)
+plt.xlabel('Frequency')
+plt.ylabel('Magnitude Spectrum')
+plt.title('Fourier Transform of Intensity')
+plt.savefig('pre_magnitude_spectrum.png')
+n=n+1
+
+plt.figure(n)
+sb.lineplot(pre_power_spectrum)
+plt.xlabel('Frequency')
+plt.ylabel('Magnitude Spectrum')
+plt.title('Fourier Transform of Intensity')
+plt.savefig('pre_power_spectrum.png')
+n=n+1
+
+plt.figure(n)
+sb.lineplot(post_magnitude_spectrum)
+plt.xlabel('Frequency')
+plt.ylabel('Magnitude Spectrum')
+plt.title('Fourier Transform of Intensity')
+plt.savefig('post_magnitude_spectrum.png')
+n=n+1
+
+plt.figure(n)
+sb.lineplot(post_power_spectrum)
+plt.xlabel('Frequency')
+plt.ylabel('Magnitude Spectrum')
+plt.title('Fourier Transform of Intensity')
+plt.savefig('post_power_spectrum.png')
+n=n+1
